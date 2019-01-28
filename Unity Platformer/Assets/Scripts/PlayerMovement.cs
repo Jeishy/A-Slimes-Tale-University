9759a1579de2b0cbdcf3dce1840ba2e615ab5d1d
kerwyndyte <kdyte1997@gmail.com>
2019-01-28 13:40:47 +0000

    Slingshot??
    A version of slingshot, forward momentum needs to be increased. Change animation if possible as well. Might have created a nice jump animation.

---
+++ Unity Platformer/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    private bool onGround;
+    private float jumpPressure;
+    private float minJump;
+    private float maxJumpPressure;
+    private Rigidbody rbody;
+
+	// Use this for initialization
+	void Start ()
+    {
+        onGround = true;
+        jumpPressure = 0f;
+        minJump = 2f;
+        maxJumpPressure = 10f;
+        rbody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    if(onGround)
+        {
+            //holding jump button
+            if(Input.GetButton("Jump"))
+            {
+                if(jumpPressure < maxJumpPressure)
+                {
+                    jumpPressure += Time.deltaTime * 10f;
+                }
+                else
+                {
+                    jumpPressure = maxJumpPressure;
+                }
+                
+            }
+            else
+            {
+                //jump
+                if (jumpPressure > 0f)
+                {
+                    jumpPressure = jumpPressure + minJump;
+                    rbody.velocity = new Vector3(jumpPressure/10f, jumpPressure, 0f);
+                    jumpPressure = 0f;
+                    onGround = false;
+                }
+            }
+        }
+	}
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.CompareTag("ground"))
+        {
+            onGround = true;
+        }
+    }
+}
