f6bf57be8b3314633feed2452b9524e78acced7d
kerwyndyte <kdyte1997@gmail.com>
2019-02-24 18:54:27 +0000

    Updated
    Updated branch. Added Shop and HubWorld.
    
    Currently Bug Fixing

---
+++ Unity Platformer/Assets/Scripts/HubWorld/Shop/GlobalShop.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalShop : MonoBehaviour {
+
+    public static string Item01;
+    public static string Item02;
+    public static string Item03;
+    public static string Item04;
+    public static string Item05;
+    public static string Item06;
+    public bool ShopHub = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (ShopHub == true)
+        {
+            
+            Item01 = "PlaceHolder1";
+            Item02 = "PlaceHolder2";
+            Item03 = "PlaceHolder3";
+            Item04 = "PlaceHolder4";
+            Item05 = "PlaceHolder5";
+            Item06 = "PlaceHolder6";
+        }
+        
+
+    }
+}
---
+++ Unity Platformer/Assets/Scripts/HubWorld/Shop/ShopHubWorld.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShopHubWorld : MonoBehaviour
+{
+    
+    
+    //Script adapated from multiple shops to a single shop. Usually would change what items are shown dependent on what shop the player goes to but in this case
+    //It just displays items for a single shop which can be edited from the Global Shop Script.
+    
+    private GameObject ShopRef;
+    public GameObject ShopInventory;
+    public GameObject Item01Text;
+    public GameObject Item02Text;
+    public GameObject Item03Text;
+    public GameObject Item04Text;
+    public GameObject Item05Text;
+    public GameObject Item06Text;
+    public GameObject ItemCompletion;
+    public GameObject CompleteText;
+
+    
+
+    private void Awake()
+    {
+        ShopRef = GameObject.Find("ShopTrigger");      
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Collision Detected");
+        GlobalShop globalShop = ShopRef.GetComponent<GlobalShop>();
+        globalShop.ShopHub = true;
+        ShopInventory.SetActive(true);
+        Item01Text.GetComponent<Text>().text = "" + GlobalShop.Item01;
+        Item02Text.GetComponent<Text>().text = "" + GlobalShop.Item02;
+        Item03Text.GetComponent<Text>().text = "" + GlobalShop.Item03;
+        Item04Text.GetComponent<Text>().text = "" + GlobalShop.Item04;
+        Item05Text.GetComponent<Text>().text = "" + GlobalShop.Item05;
+        Item06Text.GetComponent<Text>().text = "" + GlobalShop.Item06;
+
+    }
+    
+   
+    
+    //Not currently very effecient way of writing if they are sure they want to buy the item they have picked.
+
+   
+    public void Item01()
+    {
+        ItemCompletion.SetActive(true);
+        CompleteText.GetComponent<Text>().text = "Are you sure you want to buy " + GlobalShop.Item01 + "?";
+    }
+
+    public void Item02()
+    {
+        ItemCompletion.SetActive(true);
+        CompleteText.GetComponent<Text>().text = "Are you sure you want to buy " + GlobalShop.Item02 + "?";
+    }
+
+    public void Item03()
+    {
+        ItemCompletion.SetActive(true);
+        CompleteText.GetComponent<Text>().text = "Are you sure you want to buy " + GlobalShop.Item03 + "?";
+    }
+
+    public void Item04()
+    {
+        ItemCompletion.SetActive(true);
+        CompleteText.GetComponent<Text>().text = "Are you sure you want to buy " + GlobalShop.Item04 + "?";
+    }
+
+    public void Item05()
+    {
+        ItemCompletion.SetActive(true);
+        CompleteText.GetComponent<Text>().text = "Are you sure you want to buy " + GlobalShop.Item05 + "?";
+    }
+
+    public void Item06()
+    {
+        ItemCompletion.SetActive(true);
+        CompleteText.GetComponent<Text>().text = "Are you sure you want to buy " + GlobalShop.Item06 + "?";
+    }
+
+    public void CancelTransaction()
+    {
+        ItemCompletion.SetActive(false);
+    }
+
+}
