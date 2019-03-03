2c80e2677bf503f6f0f8fb7a1b0f9dee8866d123
kerwyndyte <kdyte1997@gmail.com>
2019-03-03 21:52:56 +0000

    Introduced Target Camera for the Hub World
    Pressing Q or E now changes the camera target to one of the doors (in very basic stage with no doors currently placed, just block placeholders to make sure the camera works).

--- Unity Platformer/Assets/Scripts/HubWorld/HubWorldRotation.cs
+++ Unity Platformer/Assets/Scripts/HubWorld/HubWorldRotation.cs
@@ -3,39 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class HubWorldRotation : MonoBehaviour {
-    public GameObject []TriggerPoints;
-    private Transform []TriggerPositions;
-    private GameObject target;
-    private Vector3 targetPoint;
-    private Quaternion targetRotation;
+    public GameObject DungeonDoor;
+    public GameObject FireDoor;
+    public GameObject WaterDoor;
+    public GameObject EarthDoor;
+   
 
     void Start()
     {
-        /*for (int i = 0; i < TriggerPoints.Length; i++)
-        {
-            TriggerPoints[i].GetComponent<Transform>();
-            Debug.Log(TriggerPoints[i]);
-        }*/
-
-        TriggerPositions = new Transform[TriggerPoints.Length];
-        //Debug.Log(TriggerPositions);
-
-        for (int i = 0; i < TriggerPoints.Length; i++)
-        {
-            TriggerPositions[i] = TriggerPoints[i].transform;
-            
-        }
-        Debug.Log(TriggerPositions);
-
-        //target = GameObject.FindWithTag("Player");
+        
     }
 
-    /*void Update()
-    {
-        targetPoint = new Vector3(target.transform.position.x, transform.position.y, target.transform.position.z) - transform.position;
-        targetRotation = Quaternion.LookRotation(-targetPoint, Vector3.up);
-        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * 2.0f);
-    }*/
 
 
 }
---
+++ Unity Platformer/Assets/Scripts/HubWorld/TargetCameraScript.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetCameraScript : MonoBehaviour {
+
+    public Transform target;
+    public float speed = 3f;
+    
+    private int currentTarget = 0;
+    private int previousTarget;
+    private int nextTarget;
+    //private int previousTarget;
+    Quaternion newRot;
+    Vector3 relPos;
+	
+	
+	// Update is called once per frame
+	void Update ()
+    {       
+            GetNewTarget();
+        if (target == true)
+        {
+            relPos = target.position - transform.position;
+            newRot = Quaternion.LookRotation(relPos);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, newRot, speed);
+        }    
+    }
+
+    void GetNewTarget()
+    {
+        GameObject[] possibleTargets;
+        possibleTargets = GameObject.FindGameObjectsWithTag("Door");
+        Debug.Log(possibleTargets[currentTarget]);
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (currentTarget < possibleTargets.Length)
+            {
+                if(currentTarget == possibleTargets.Length - 1)
+                { 
+                    return;
+                }
+                currentTarget++;
+                nextTarget = currentTarget;
+                
+            }
+            
+        }
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            if (currentTarget > 0)
+            {
+
+                currentTarget--;
+                nextTarget = currentTarget;
+
+            }
+        }
+        
+            target = possibleTargets[nextTarget].transform;
+
+        
+    }
+
+}
