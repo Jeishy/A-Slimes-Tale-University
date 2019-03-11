85ff3f76c560f80512ec5be6d7d2a9ba12acae57
kerwyndyte <kdyte1997@gmail.com>
2019-03-11 13:14:48 +0000

    Text Trigger WIP

---
+++ Unity Platformer/Assets/Scripts/TextOnTrigger.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextOnTrigger : MonoBehaviour {
+
+    public Text text;
+    private bool textFading;
+
+    
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision == GameObject.FindGameObjectWithTag("Player"))
+        {
+            textFading = true;
+            text.CrossFadeAlpha(1, 2f, false);
+        }
+        /*else
+        {
+            textFading = false;
+            text.CrossFadeAlpha(0, 2f, false);
+        }*/
+    }
+
+    public void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision == GameObject.FindGameObjectWithTag("Player"))
+        {
+            textFading = false;
+            text.CrossFadeAlpha(0, 2f, false);
+        }
+    }
+}
