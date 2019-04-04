9a923e429e427c830963e54754ede34c6d36bd81
Jeishy <k1519236@kingston.ac.uk>
2019-04-04 09:06:44 +0100

    Implemented working settings menu in main menu

---
+++ Unity Platformer/Assets/Scripts/UI/Main Menu/Settings.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Settings : MonoBehaviour {
+
+    private AudioManager _audioManager;
+    private float _origVolume;
+
+    private void Start()
+    {
+        _audioManager = AudioManager.instance;
+        _origVolume = AudioListener.volume;
+    }
+
+    public void OnMusicToggle(bool toggle)
+    {
+        if (!toggle)
+            AudioListener.volume = 0f;
+        else
+            AudioListener.volume = _origVolume;
+    }
+
+    public void OnMasterVolumeChange(float volume)
+    {
+        AudioListener.volume = volume;
+        Debug.Log(AudioListener.volume);
+    }
+
+    public void OnMusicVolumeChange(float volume)
+    {
+
+    }
+}
