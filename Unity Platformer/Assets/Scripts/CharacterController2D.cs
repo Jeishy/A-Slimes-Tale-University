d85d323d03da38a74b988ca3665f458d6cda27a0
Axel Jaworski <axeljaworski98@gmail.com>
2019-04-04 20:14:42 +0100

    Added comments

--- Unity Platformer/Assets/Scripts/CharacterController2D.cs
+++ Unity Platformer/Assets/Scripts/CharacterController2D.cs
@@ -5,156 +5,156 @@ using UnityEngine.Events;
 
 public class CharacterController2D : MonoBehaviour
 {
-	[SerializeField] private float m_JumpForce = 400f;							// Amount of force added when the player jumps.
-	[Range(0.25f, 1)] [SerializeField] private float m_WallJumpVerticalForceMultiplier = 0.3f;	// Amount the jump force is multiplied by when jumping off a wall
-	[SerializeField] private float m_HorizontalJumpForce = 200f;				// Amount of lateral force added when the player jumps from a wall
-	[SerializeField] private float m_MaxWallSlideSpeed;							// Maximum wall sliding speed
-	[SerializeField] private LayerMask m_WallLayer;                             // Layer of the walls
-	[Range(0, 1)] [SerializeField] private float m_CrouchSpeed = .36f;			// Amount of maxSpeed applied to crouching movement. 1 = 100%
-	[Range(0, 2f)] [SerializeField] private float m_GroundMovementSmoothing = .05f;	// How much to smooth out the movement
-	[Range(0, 2f)] [SerializeField] private float m_AirMovementSmoothing = .05f;	// How much to smooth out the movement
-	[SerializeField] private bool m_AirControl = false;							// Whether or not a player can steer while jumping;
-	public LayerMask m_WhatIsGround;							                // A mask determining what is ground to the character
-	[SerializeField] private Transform m_GroundCheck;							// A position marking where to check if the player is grounded.
-	[SerializeField] private Transform m_CeilingCheck;							// A position marking where to check for ceilings
-	[SerializeField] private Transform m_WallCheck;								// Transform of a wall check object
-    [SerializeField] private float m_wallDetectRadius = 0.1f;					// Radius of a sphere cast
-	[SerializeField] private Collider m_CrouchDisableCollider;				    // A collider that will be disabled when crouching
-	[SerializeField] private Vector2 m_KnockbackForce;							// Force applied when hit by an enemy
-	[SerializeField] private float m_KnockbackLength;							// Period of time the player will not be able to move after getting knocked bakck
+    [SerializeField] private float m_JumpForce = 400f;                          // Amount of force added when the player jumps.
+    [Range(0.25f, 1)] [SerializeField] private float m_WallJumpVerticalForceMultiplier = 0.3f;  // Amount the jump force is multiplied by when jumping off a wall
+    [SerializeField] private float m_HorizontalJumpForce = 200f;                // Amount of lateral force added when the player jumps from a wall
+    [SerializeField] private float m_MaxWallSlideSpeed;                         // Maximum wall sliding speed
+    [SerializeField] private LayerMask m_WallLayer;                             // Layer of the walls
+    [Range(0, 1)] [SerializeField] private float m_CrouchSpeed = .36f;          // Amount of maxSpeed applied to crouching movement. 1 = 100%
+    [Range(0, 2f)] [SerializeField] private float m_GroundMovementSmoothing = .05f; // How much to smooth out the movement
+    [Range(0, 2f)] [SerializeField] private float m_AirMovementSmoothing = .05f;    // How much to smooth out the movement
+    [SerializeField] private bool m_AirControl = false;                         // Whether or not a player can steer while jumping;
+    public LayerMask m_WhatIsGround;                                            // A mask determining what is ground to the character
+    [SerializeField] private Transform m_GroundCheck;                           // A position marking where to check if the player is grounded.
+    [SerializeField] private Transform m_CeilingCheck;                          // A position marking where to check for ceilings
+    [SerializeField] private Transform m_WallCheck;								// Transform of a wall check object
+    [SerializeField] private float m_wallDetectRadius = 0.1f;                   // Radius of a sphere cast
+    [SerializeField] private Collider m_CrouchDisableCollider;                  // A collider that will be disabled when crouching
+    [SerializeField] private Vector2 m_KnockbackForce;                          // Force applied when hit by an enemy
+    [SerializeField] private float m_KnockbackLength;							// Period of time the player will not be able to move after getting knocked bakck
     [SerializeField] private GameObject m_WallJumpEffect;                       // Jumping particle effect
-	
 
-	const float k_GroundedRadius = .2f;                                         // Radius of the overlap circle to determine if grounded
-	[HideInInspector] public bool m_Grounded;                                   // Whether or not the player is grounded.
-	private bool m_wallSliding;			                                        // Whether or not the player is touching the wall
-	const float k_CeilingRadius = .2f;                                          // Radius of the overlap circle to determine if the player can stand up
-	private Rigidbody m_Rigidbody;                                              // Rigidbody of the player
-	private bool m_FacingRight = true;                                          // For determining which way the player is currently facing.
-	private Vector3 m_Velocity = Vector3.zero;                                  // Velocity variable used to calculate movement
-	private float m_WallNormal;                                                 // Stores the normal of the last touched wall
-	private bool m_WallJumped = false;                                          // Is set to true when player has jumped from a wall and is in air
-	
-	private float m_KnockbackCount;                                             // Cooldown timer for the knockback effect when hit
-	private bool m_KnockbackRight;		                                        // Used to determine direction when applying knockback force
+
+    const float k_GroundedRadius = .2f;                                         // Radius of the overlap circle to determine if grounded
+    [HideInInspector] public bool m_Grounded;                                   // Whether or not the player is grounded.
+    private bool m_wallSliding;                                                 // Whether or not the player is touching the wall
+    const float k_CeilingRadius = .2f;                                          // Radius of the overlap circle to determine if the player can stand up
+    private Rigidbody m_Rigidbody;                                              // Rigidbody of the player
+    private bool m_FacingRight = true;                                          // For determining which way the player is currently facing.
+    private Vector3 m_Velocity = Vector3.zero;                                  // Velocity variable used to calculate movement
+    private float m_WallNormal;                                                 // Stores the normal of the last touched wall
+    private bool m_WallJumped = false;                                          // Is set to true when player has jumped from a wall and is in air
+
+    private float m_KnockbackCount;                                             // Cooldown timer for the knockback effect when hit
+    private bool m_KnockbackRight;		                                        // Used to determine direction when applying knockback force
     private Vector3 m_TouchedWallPoint;                                         // Position of the point on the last touched wall
     private float m_FlipDelay;                                                  // Flip delay counter
-	private AudioManager _audioManager;                                         // Audio Manager reference
-    
-	[Header("Events")]
-	[Space]
+    private AudioManager _audioManager;                                         // Audio Manager reference
 
-	public UnityEvent OnLandEvent;
+    [Header("Events")]
+    [Space]
 
-	[System.Serializable]
-	public class BoolEvent : UnityEvent<bool> { }
+    public UnityEvent OnLandEvent;
 
-	public BoolEvent OnCrouchEvent;
-	private bool m_wasCrouching = false;
+    [System.Serializable]
+    public class BoolEvent : UnityEvent<bool> { }
 
-	private void Awake()
-	{
-		m_Rigidbody = GetComponent<Rigidbody>();                                // Gets the rigidbody component from the player
-		_audioManager = AudioManager.instance;                                  // Gets the instance of the AudioManager
+    public BoolEvent OnCrouchEvent;
+    private bool m_wasCrouching = false;
 
-		if (OnLandEvent == null)
-			OnLandEvent = new UnityEvent();
+    private void Awake()
+    {
+        m_Rigidbody = GetComponent<Rigidbody>();                                // Gets the rigidbody component from the player
+        _audioManager = AudioManager.instance;                                  // Gets the instance of the AudioManager
 
-		if (OnCrouchEvent == null)
-			OnCrouchEvent = new BoolEvent();
-		
-	}
+        if (OnLandEvent == null)
+            OnLandEvent = new UnityEvent();
+
+        if (OnCrouchEvent == null)
+            OnCrouchEvent = new BoolEvent();
+
+    }
 
     //Fixed update is used when working with rigidbodies and forces
-	private void FixedUpdate()
-	{
+    private void FixedUpdate()
+    {
 
         //Flag variables 
-		bool wasGrounded = m_Grounded;
-		m_Grounded = false;
-		m_WallJumped = false;
+        bool wasGrounded = m_Grounded;
+        m_Grounded = false;
+        m_WallJumped = false;
 
-		// The player is grounded if a circlecast to the groundcheck position hits anything designated as ground
-		// This can be done using layers instead but Sample Assets will not overwrite your project settings.
-		Collider[] colliders = Physics.OverlapSphere(m_GroundCheck.position, k_GroundedRadius, m_WhatIsGround);
-		for (int i = 0; i < colliders.Length; i++)
-		{
-			if (colliders[i].gameObject != gameObject)
-			{
-				m_Grounded = true;
-				if (!wasGrounded)
-					OnLandEvent.Invoke();
-			}
-		}	
-	}
+        // The player is grounded if a circlecast to the groundcheck position hits anything designated as ground
+        // This can be done using layers instead but Sample Assets will not overwrite your project settings.
+        Collider[] colliders = Physics.OverlapSphere(m_GroundCheck.position, k_GroundedRadius, m_WhatIsGround);
+        for (int i = 0; i < colliders.Length; i++)
+        {
+            if (colliders[i].gameObject != gameObject)
+            {
+                m_Grounded = true;
+                if (!wasGrounded)
+                    OnLandEvent.Invoke();
+            }
+        }
+    }
 
     //Called from the PlayerControls.cs class where input is retrieved 
-	public void Move(float move, bool jump)
-	{
+    public void Move(float move, bool jump)
+    {
 
-		//only control the player if grounded or airControl is turned on
-		if (m_Grounded || m_AirControl)
-		{
+        //only control the player if grounded or airControl is turned on
+        if (m_Grounded || m_AirControl)
+        {
 
-            
-			Vector3 targetVelocity;
+
+            Vector3 targetVelocity;
             //If knockback effect has not been applied
             if (m_KnockbackCount <= 0)
-			{
-				// Move the character by finding the target velocity
-				targetVelocity = new Vector2(move * 10f, m_Rigidbody.velocity.y);
-			}
+            {
+                // Move the character by finding the target velocity
+                targetVelocity = new Vector2(move * 10f, m_Rigidbody.velocity.y);
+            }
             //Else apply the knockback effect
-			else
-			{
+            else
+            {
                 //To the right
-				if (m_KnockbackRight)
-				{
-					targetVelocity = new Vector2(-m_KnockbackForce.x, m_KnockbackForce.y);
-				}
+                if (m_KnockbackRight)
+                {
+                    targetVelocity = new Vector2(-m_KnockbackForce.x, m_KnockbackForce.y);
+                }
                 //To the left
-				else
-				{
-					targetVelocity = new Vector2(m_KnockbackForce.x, m_KnockbackForce.y);
-				}
+                else
+                {
+                    targetVelocity = new Vector2(m_KnockbackForce.x, m_KnockbackForce.y);
+                }
 
                 //Decrease knockback over time
-				m_KnockbackCount -= Time.deltaTime;
-			}
+                m_KnockbackCount -= Time.deltaTime;
+            }
 
-			// Gruond movement smoothing
-			if (m_Grounded) 
-				m_Rigidbody.velocity = Vector3.SmoothDamp(m_Rigidbody.velocity, targetVelocity, ref m_Velocity, m_GroundMovementSmoothing);
+            // Gruond movement smoothing
+            if (m_Grounded)
+                m_Rigidbody.velocity = Vector3.SmoothDamp(m_Rigidbody.velocity, targetVelocity, ref m_Velocity, m_GroundMovementSmoothing);
 
 
             // Air movement smoothing
-			if (!m_Grounded)
-			{
-				m_Rigidbody.velocity = Vector3.SmoothDamp(m_Rigidbody.velocity, targetVelocity, ref m_Velocity, m_AirMovementSmoothing);
-			}
-			
-			// If the input is moving the player right and the player is facing left...
-			if (move > 0 && !m_FacingRight)
-			{
-				// ... flip the player.
-				Flip();
+            if (!m_Grounded)
+            {
+                m_Rigidbody.velocity = Vector3.SmoothDamp(m_Rigidbody.velocity, targetVelocity, ref m_Velocity, m_AirMovementSmoothing);
+            }
+
+            // If the input is moving the player right and the player is facing left...
+            if (move > 0 && !m_FacingRight)
+            {
+                // ... flip the player.
+                Flip();
 
             }
             // Otherwise if the input is moving the player left and the player is facing right...
             else if (move < 0 && m_FacingRight)
-			{
-				// ... flip the player.
-				Flip();
+            {
+                // ... flip the player.
+                Flip();
 
             }
         }
 
         // Limit player wall slide speed to value of 'm_MaxWallSlideSpeed'
         if (m_wallSliding && m_Rigidbody.velocity.y <= -m_MaxWallSlideSpeed)
-		{
-			m_Rigidbody.velocity = new Vector2(m_Rigidbody.velocity.x, -m_MaxWallSlideSpeed);
+        {
+            m_Rigidbody.velocity = new Vector2(m_Rigidbody.velocity.x, -m_MaxWallSlideSpeed);
 
-		}
+        }
 
 
         //Unstick from wall whenever player tries to move away from the wall
@@ -168,47 +168,48 @@ public class CharacterController2D : MonoBehaviour
 
         // If the player should jump...
         if (((m_Grounded || m_wallSliding) && jump))
-		{
-			
-			StickToWall(false);
+        {
+
+            StickToWall(false);
             m_Grounded = false;
 
             //Play jump sound
-			_audioManager.Play("PlayerJump");
+            _audioManager.Play("PlayerJump");
 
-			// ... and player is touching the wall
-			if (m_wallSliding && !m_Grounded)
-			{   
-				// Add a vertical and horizontal force to the player if he is sliding on the wall
-				m_Rigidbody.AddForce(new Vector2((m_HorizontalJumpForce * m_WallNormal) , (m_JumpForce * m_WallJumpVerticalForceMultiplier)));
-				m_WallJumped = true;
+            // ... and player is touching the wall
+            if (m_wallSliding && !m_Grounded)
+            {
+                // Add a vertical and horizontal force to the player if he is sliding on the wall
+                m_Rigidbody.AddForce(new Vector2((m_HorizontalJumpForce * m_WallNormal), (m_JumpForce * m_WallJumpVerticalForceMultiplier)));
+                m_WallJumped = true;
                 // Display wall jumping particle effect
                 GameObject pWallJump = Instantiate(m_WallJumpEffect, m_TouchedWallPoint, Quaternion.identity);
                 Destroy(pWallJump, 1f);
-				// Flip because character will jump off the wall in the other direction
-				Flip();
-			}
-			
-			// ... and player is not touching the wall
-			else
-			{
-				// Add only the vertical force to the player.
-				m_Rigidbody.AddForce(new Vector2(0f, m_JumpForce), ForceMode.Force);
-			}
+                // Flip because character will jump off the wall in the other direction
+                Flip();
+            }
 
-		}
-	}
+            // ... and player is not touching the wall
+            else
+            {
+                // Add only the vertical force to the player.
+                m_Rigidbody.AddForce(new Vector2(0f, m_JumpForce), ForceMode.Force);
+            }
+
+        }
+    }
 
     //Sets flags for the knockback system above
-	public void Knockback(bool right)
-	{
-		m_KnockbackRight = right;
-		m_KnockbackCount = m_KnockbackLength;
-	}
+    public void Knockback(bool right)
+    {
+        m_KnockbackRight = right;
+        m_KnockbackCount = m_KnockbackLength;
+    }
 
 
     //Returns -1 if moving left, 0 if stationary and 1 if moving right
-    private int GetRoundedInputs() {
+    private int GetRoundedInputs()
+    {
         int input = 0;
         if (Input.GetAxis("Horizontal") < 0)
             input = -1;
@@ -221,9 +222,9 @@ public class CharacterController2D : MonoBehaviour
 
     //Flips the character by 180 degrees and also flips the scale of the object
     private void Flip()
-	{
+    {
         //Small delayed required between flips, otherwise bugs may occur
-        if (m_FlipDelay <= Time.time) 
+        if (m_FlipDelay <= Time.time)
         {
             //Make sure the player doesnt flip when is already in the good position whilst wall sliding
             if (m_wallSliding && (m_FacingRight && m_WallNormal == 1) || (!m_FacingRight && m_WallNormal == -1))
@@ -238,15 +239,15 @@ public class CharacterController2D : MonoBehaviour
             transform.localScale = theScale;
             m_FlipDelay = 0.1f + Time.time;
         }
-	}
- 
+    }
+
     //This is called whenever the player collides with a wall
     private void StickToWall(bool stick)
     {
 
         //Freezes rigidbodies constraints to prevent moving horizontally whilst stuck on the wall
         if (stick)
-            m_Rigidbody.constraints =     RigidbodyConstraints.FreezePositionX |
+            m_Rigidbody.constraints = RigidbodyConstraints.FreezePositionX |
                                             RigidbodyConstraints.FreezeRotation;
         //Unstick from the wall
         else
@@ -255,31 +256,31 @@ public class CharacterController2D : MonoBehaviour
         }
     }
 
-	private void OnCollisionEnter(Collision other)
-	{
+    private void OnCollisionEnter(Collision other)
+    {
         //If the player collided with the wall...
-		if (other.gameObject.CompareTag("Wall"))
-		{
+        if (other.gameObject.CompareTag("Wall"))
+        {
             //Store the wall normal
-			m_WallNormal = other.contacts[0].normal.x;
-            
-	         //Possible alternative fix for poor wall jumping
+            m_WallNormal = other.contacts[0].normal.x;
+
+            //Possible alternative fix for poor wall jumping
             if (Mathf.Abs(m_WallNormal) == 1)
             {
                 //Set wall sliding flag to true and stick to wall
                 m_wallSliding = true;
                 StickToWall(true);
             }
-		}
-	}
+        }
+    }
 
     private void OnCollisionStay(Collision other)
     {
         m_TouchedWallPoint = other.contacts[0].point;       // Getting the point last touched for the jump particle effect
     }
 
-	private void OnCollisionExit(Collision other)
-	{
+    private void OnCollisionExit(Collision other)
+    {
         //Unstick from wall
         m_wallSliding = false;
         StickToWall(false);
