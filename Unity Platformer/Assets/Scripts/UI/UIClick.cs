7822d65aaf31125840c0356a79b65cedd1421248
Jeishy <k1519236@kingston.ac.uk>
2019-04-04 07:40:43 +0100

    Added UI click sound

---
+++ Unity Platformer/Assets/Scripts/UI/UIClick.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIClick : MonoBehaviour {
+
+    private AudioManager _audioManager;
+
+    private void Start()
+    {
+        _audioManager = AudioManager.instance;
+    }
+
+    public void PlayUIClickSound()
+    {
+        _audioManager.Play("UIClick");
+    }
+}
