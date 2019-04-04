c59951975cc3b2cee1b7dc8ae3152e8033946180
Jeishy <k1519236@kingston.ac.uk>
2019-04-04 05:58:23 +0100

    Added coins collected UI element

---
+++ Unity Platformer/Assets/Scripts/UI/ShowCoinsCollected.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ShowCoinsCollected : MonoBehaviour {
+
+    private TextMeshProUGUI _coinsCollectedTxt;
+
+    private void Start()
+    {
+        _coinsCollectedTxt = GetComponent<TextMeshProUGUI>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        _coinsCollectedTxt.text = "Coins Collected: " + GameManager.instance.collectibles;
+
+    }
+}
