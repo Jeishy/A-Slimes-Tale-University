9efccba27734074ffcf67492de66d240cc6b177d
Jeishy <k1519236@kingston.ac.uk>
2019-07-08 23:53:21 +0100

    Fixed bug with fall down movement, started adding beginning images to cutscene

--- Unity Platformer/Assets/Scripts/Chapter 0 Events/FallDown.cs
+++ Unity Platformer/Assets/Scripts/Chapter 0 Events/FallDown.cs
@@ -21,21 +21,23 @@ public class FallDown : MonoBehaviour
     private IEnumerator Slip()
     {
         _charController.enabled = false;
+        _hunterMovement.enabled = false;
         foreach (Transform slipTrans in _slipPosTrans)
         {
             Vector3 slipPos = slipTrans.position;
             float time = 0f;
             bool _isPointReached = false;
-            while (time <= 1.0f && !_isPointReached)
+            while (time <= 1.4f && !_isPointReached)
             {
                 time += Time.deltaTime;
-                _hunterTrans.position = Vector3.MoveTowards(_hunterTrans.position, slipPos, 0.25f);
+                _hunterTrans.position = Vector3.MoveTowards(_hunterTrans.position, slipPos, 0.27f);
                 float distance = Vector3.Distance(_hunterTrans.position, slipPos);
                 if (Mathf.Abs(distance) < 0.05f)
                     _isPointReached = true;
                 yield return null;
             }
         }
+        _hunterMovement.enabled = true;
         _hunterMovement._anim.SetTrigger("Fallen");
         _charController.enabled = true;
     }
